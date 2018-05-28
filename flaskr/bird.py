a7164bf9017e100198dff6ad0eb3396a7beed052
amj133 <amj@vt.edu>
2018-05-28 16:14:35 -0600

    create bird class

---
+++ flaskr/bird.py
@@ -0,0 +1,5 @@
+class Bird(object):
+    def __init__(self, com_name, sci_name, species_code):
+        self.com_name = com_name
+        self.sci_name = sci_name
+        self.species_code = species_code 
