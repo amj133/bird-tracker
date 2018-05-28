fe22948beec9ab58cc31a56f1ff901563d330941
amj133 <amj@vt.edu>
2018-05-28 16:12:11 -0600

    update create favorite endpoint to insert common name species name and species code into db

--- flaskr/favorites_endpoints.py
+++ flaskr/favorites_endpoints.py
@@ -10,16 +10,20 @@ bp = Blueprint('favorites_endpoints', __name__, url_prefix='/api/v1')
 @bp.route('/favorites', methods=['POST'])
 @login_required
 def create():
-    species_codes = request.form.getlist('birds[]')
+    bird_info = request.form.getlist('birds[]')
     db = get_db()
     user_id = g.user['id']
+    ipdb.set_trace()
 
-    for code in species_codes:
+    for info in bird_info:
+        species_code = info.split("/")[0]
+        common_name = info.split("/")[1]
+        sci_name = info.split("/")[2]
         db.execute(
-            'INSERT INTO bird (species_code) VALUES (?)', (code.encode('utf8'),)
+            'INSERT INTO bird (species_code, common_name, sci_name) VALUES (?, ?, ?)', (species_code.encode('utf8'), common_name, sci_name)
         )
         bird_id = db.execute(
-            'SELECT bird.id FROM bird WHERE species_code = ?', (code.encode('utf8'),)
+            'SELECT bird.id FROM bird WHERE species_code = ?', (species_code.encode('utf8'),)
         ).fetchone()
         db.execute(
             'INSERT INTO user_birds (user_id, bird_id) VALUES (?, ?)', (int(user_id), bird_id[0])
