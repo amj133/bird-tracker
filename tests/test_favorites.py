d8e2d995a445bf36652a97c7a90ce81c73cda2ed
amj133 <amj@vt.edu>
2018-05-29 15:02:40 -0600

    add user favorite show page has recent sightings - test passing

--- tests/test_favorites.py
+++ tests/test_favorites.py
@@ -10,7 +10,7 @@ def test_user_favorites_none_if_not_added(client, auth):
     assert response.status_code == 200
     assert "Go to search to add favorite birds!" in response.data
 
-def test_user_favorites_displayed(client, auth, app):
+def test_user_favorites_displayed_in_index(client, auth, app):
     res = auth.login()
     with app.app_context():
         get_db().execute("INSERT INTO bird (species_code, common_name, sci_name) VALUES ('bgbrd', 'Big Bird', 'Birdus largus')")
@@ -19,6 +19,19 @@ def test_user_favorites_displayed(client, auth, app):
         assert 'Big Bird' in response.data
         assert 'Birdus largus' in response.data
         assert 'bgbrd' in response.data
+
+def test_user_favorites_show_page_has_recent_sightings(client, auth, app):
+    res = auth.login()
+    with app.app_context():
+        get_db().execute("INSERT INTO bird (species_code, common_name, sci_name) VALUES ('bgbrd', 'Big Bird', 'Birdus largus')")
+        get_db().execute('INSERT INTO user_birds (user_id, bird_id) VALUES (1, 1)')
+        response = client.get('/favorites/1')
+        assert 'Big Bird' in response.data
+        assert 'Birdus largus' in response.data
+        assert 'Scientific Name' in response.data
+        assert 'Observation Date' in response.data
+        assert 'Location' in response.data
+        assert 'How Many' in response.data
 
 def test_user_favorites_search(client, auth):
     res = auth.login()
