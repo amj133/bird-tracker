f95c09654a8d7bca8ba36268a9fb1ee83f91189e
amj133 <amj@vt.edu>
2018-05-29 15:53:31 -0600

    create user favorites delete removes user bird association

--- tests/test_favorites.py
+++ tests/test_favorites.py
@@ -33,6 +33,18 @@ def test_user_favorites_show_page_has_recent_sightings(client, auth, app):
         assert 'Location' in response.data
         assert 'How Many' in response.data
 
+def test_user_favorites_delete_removes_bird_user_association(client, auth, app):
+    res = auth.login()
+    with app.app_context():
+        get_db().execute("INSERT INTO bird (species_code, common_name, sci_name) VALUES ('bgbrd', 'Big Bird', 'Birdus largus')")
+        get_db().execute('INSERT INTO user_birds (user_id, bird_id) VALUES (1, 1)')
+        response = client.get('/favorites/')
+        assert 'Big Bird' in response.data
+
+        client.post('/favorites/1/delete')
+        response = client.get('/favorites/')
+        assert 'Big Bird' not in response.data
+
 def test_user_favorites_search(client, auth):
     res = auth.login()
     response = client.get('/favorites/search')
