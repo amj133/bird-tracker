ebd7af782dfe7eded5e09c50efac4f4b68f1e025
amj133 <amj@vt.edu>
2018-06-02 08:55:11 -0600

    update faovrite endpoints to have sqlalchemy compatible queries

--- birdy/controllers/favorites_endpoints.py
+++ birdy/controllers/favorites_endpoints.py
@@ -7,6 +7,7 @@ from birdy.controllers.auth import login_required
 from birdy.db import get_db
 from flask_mail import Mail, Message
 from birdy.services.mail_generator import MailGenerator
+from sqlalchemy import text
 
 bp = Blueprint('favorites_endpoints', __name__, url_prefix='/api/v1')
 
@@ -15,23 +16,33 @@ bp = Blueprint('favorites_endpoints', __name__, url_prefix='/api/v1')
 @login_required
 def create():
     bird_info = request.form.getlist('birds[]')
-    db = get_db()
+    # db = get_db()
     user_id = g.user['id']
 
     for info in bird_info:
-        species_code = info.split("/")[0]
+        species_code = info.split("/")[0].encode('utf8')
         common_name = info.split("/")[1]
         sci_name = info.split("/")[2]
-        db.execute(
-            'INSERT INTO bird (species_code, common_name, sci_name) VALUES (?, ?, ?)', (species_code.encode('utf8'), common_name, sci_name)
-        )
-        bird_id = db.execute(
-            'SELECT bird.id FROM bird WHERE species_code = ?', (species_code.encode('utf8'),)
-        ).fetchone()
-        db.execute(
-            'INSERT INTO user_birds (user_id, bird_id) VALUES (?, ?)', (int(user_id), bird_id[0])
-        )
-        db.commit()
+        query = text("INSERT INTO bird (species_code, common_name, sci_name) VALUES (:species_code, :common_name, :sci_name)")
+        query = query.bindparams(species_code=species_code, common_name=common_name, sci_name=sci_name)
+        get_db().engine.execute(query)
+        query = text("SELECT bird.id FROM bird WHERE species_code = :species_code")
+        query = query.bindparams(species_code=species_code)
+        bird_id = get_db().engine.execute(query).fetchone()
+        # import ipdb; ipdb.set_trace()
+        query = text("INSERT INTO user_birds (user_id, bird_id) VALUES (:user_id, :bird_id)")
+        query = query.bindparams(user_id=int(user_id), bird_id=bird_id[0])
+        get_db().engine.execute(query)
+        # db.execute(
+        #     'INSERT INTO bird (species_code, common_name, sci_name) VALUES (?, ?, ?)', (species_code.encode('utf8'), common_name, sci_name)
+        # )
+        # bird_id = db.execute(
+        #     'SELECT bird.id FROM bird WHERE species_code = ?', (species_code.encode('utf8'),)
+        # ).fetchone()
+        # db.execute(
+        #     'INSERT INTO user_birds (user_id, bird_id) VALUES (?, ?)', (int(user_id), bird_id[0])
+        # )
+        # db.commit()
 
     return "Favorites Added"
 
