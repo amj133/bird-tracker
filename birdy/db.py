b1d659de3cce0df13543bb190f81f93563043be1
amj133 <amj@vt.edu>
2018-06-02 09:48:35 -0600

    update #get_db and close_db to be pg compatible

--- birdy/db.py
+++ birdy/db.py
@@ -3,20 +3,10 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 from flask_sqlalchemy import SQLAlchemy
-from birdy import create_db
 
 def get_db():
     if 'db' not in g:
-        # import ipdb; ipdb.set_trace()
-
         g.db = current_app.db
-        # g.db = SQLAlchemy(current_app)
-
-        # g.db = sqlite3.connect(
-        #     current_app.config['DATABASE'],
-        #     detect_types=sqlite3.PARSE_DECLTYPES
-        # )
-        # g.db.row_factory = sqlite3.Row
 
     return g.db
 
@@ -25,14 +15,12 @@ def close_db(e=None):
 
     if db is not None:
         db.engine.dispose()
-        # db.close()
 
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.engine.execute(f.read().decode('utf8'))
-        # db.executescript(f.read().decode('utf8'))
 
 @click.command('init-db')
 @with_appcontext
