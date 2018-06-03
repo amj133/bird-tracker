5215515d29c342950e1187ef15ddc255496d2a98
amj133 <amj@vt.edu>
2018-06-03 15:11:04 -0600

    update auth register action and view to include email

--- birdy/db.py
+++ birdy/db.py
@@ -1,4 +1,3 @@
-import sqlite3
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
@@ -25,7 +24,6 @@ def init_db():
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-    """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
 
