bc073dc700ca2744e3e24bcf15575e9475d03822
amj133 <amj@vt.edu>
2018-06-04 10:48:29 -0600

    create celeryconfig file with celerybeat schedlules for jobs send dail, weekly, monthly favorite sightings emails

--- celeryconfig.py
+++ celeryconfig.py
@@ -3,9 +3,19 @@ from celery.schedules import crontab
 CELERY_IMPORTS = ('birdy.jobs.background_jobs')
 CELERY_TIMEZONE = 'UTC'
 CELERYBEAT_SCHEDULE = {
-    'send_fav_sightings_emails': {
-        'task': 'birdy.jobs.background_jobs.send_nightly_emails',
-        'schedule': crontab(minute=0, hour=7),
+    'send_daily_fav_sightings_emails': {
+        'task': 'birdy.jobs.background_jobs.send_daily_sightings_emails',
+        'schedule': crontab(),
         'args': ()
     },
+    'send_weekly_fav_sightings_emails': {
+        'task': 'birdy.jobs.background_jobs.send_weekly_sightings_emails',
+        'schedule': crontab(day_of_week=5, hour=12),
+        'args': ()
+    },
+    'send_monthly_fav_sightings_emails': {
+        'task': 'birdy.jobs.background_jobs.send_monthly_sightings_emails',
+        'schedule': crontab(0, 0, day_of_month='1'),
+        'args': ()
+    }
 }
